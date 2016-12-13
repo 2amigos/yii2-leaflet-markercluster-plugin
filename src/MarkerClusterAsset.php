b51a6045e540dc3e8092e287068f70c01ae8ae56
vladim1 <shev.vladim111@gmail.com>
2016-12-13 09:56:06 +0200

    Js and css files should better use bower directory

--- src/MarkerClusterAsset.php
+++ src/MarkerClusterAsset.php
@@ -21,8 +21,8 @@ class MarkerClusterAsset extends AssetBundle
 {
 
     public $css = [
-        'css/MarkerCluster.css',
-        'css/MarkerCluster.Default.css'
+        'MarkerCluster.css',
+        'MarkerCluster.Default.css'
     ];
 
     public $depends = [
@@ -32,7 +32,7 @@ class MarkerClusterAsset extends AssetBundle
 
     public function init()
     {
-        $this->sourcePath = __DIR__ . '/assets';
-        $this->js = YII_DEBUG ? ['js/leaflet.markercluster-src.js'] : ['js/leaflet.markercluster.js'];
+        $this->sourcePath = \Yii::getAlias('@bower').'/leaflet.markercluster/dist';
+        $this->js = YII_DEBUG ? ['leaflet.markercluster-src.js'] : ['leaflet.markercluster.js'];
     }
 }
